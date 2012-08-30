d1005dd467c214610bf21e05df3c6515713b56d8
Raymond John Hill <raymondjohnhill@gmail.com>
2012-08-30 14:47:00 -0400

    Finished explode code, removed stuff I didn't need… time to test.

--- src/com/omfgp/explodr/Main.java
+++ src/com/omfgp/explodr/Main.java
@@ -14,7 +14,6 @@ public class Main extends JavaPlugin {
 	
 	Logger theLog;
 	private Player thePlayer = null;
-	private World theWorld = null;
 	
 	public void onEnable() {
 		this.theLog = this.getLogger();
@@ -26,15 +25,31 @@ public class Main extends JavaPlugin {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
-		//Check if the sender is a player.
+		//check if the sender is a player
 		if(sender instanceof Player) {
-			this.thePlayer = (Player) sender;
+			this.thePlayer = (Player)sender;
 		}
 		
+		//if the /explodr command is sent
 		if(cmd.getName().equalsIgnoreCase("Explodr")) {
-
+			//get players location
+			Location pLoc = this.thePlayer.getLocation();
+			
+			//make a radius
+			int radius = 2;
+			int nradius = radius*(-1);
+			
+			//make an explosion in that radius
+			for(int x=nradius; x<=radius; x++) {
+				for(int y=nradius; y<=radius; y++) {
+					for(int z=nradius; z<=radius; z++) {
+						this.thePlayer.getWorld().createExplosion(pLoc.getX()+x, pLoc.getY()+y, pLoc.getZ()+z, 4.0F, true);
+					}
+				}
+			}
 		}
 		
+		//if the /findr command is sent
 		if(cmd.getName().equalsIgnoreCase("Findr")) {
 			this.thePlayer.sendMessage(ChatColor.GREEN + "Your location is: [ "
 													   + this.thePlayer.getLocation().getX() + ", "
