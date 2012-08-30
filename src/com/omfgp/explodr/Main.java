c5f8a95237b3eba1887129c1845004643b2f4670
Raymond John Hill <raymondjohnhill@gmail.com>
2012-08-30 14:40:02 -0400

    Added onEnable(), onDisable(), and onCommand() methods. Also added a world and a player reference and a logger.

--- src/com/omfgp/explodr/Main.java
+++ src/com/omfgp/explodr/Main.java
@@ -1,7 +1,29 @@
 package com.omfgp.explodr;
 
+import java.util.logging.Logger;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	Logger theLog;
+	private Player thePlayer = null;
+	private World theWorld = null;
+	
+	public void onEnable() {
+		this.theLog = this.getLogger();
+		this.theLog.info("Explodr enabled.");		
+	}
+ 
+	public void onDisable() {
+		this.theLog.info("Explodr disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
+		
+	}
 
 }
